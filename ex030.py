5514bcb993428bdd7c7e320115d54487df94b562
JungDoHyeong <ehgud7747@naver.com>
2020-12-07 14:23:56 +0900

    python

--- ex030.py
+++ ex030.py
@@ -1,3 +1,3 @@
 string = 'abcd' # 문자열 자료형
-string.replace('b','B') #replace 를 사용하면 위의 문자열을 변하는 게 아니라 새로운 문자열을 생성합니다.
-print(string) # 따라서 abxd 그대로 출력된다.
+string.replace('b','B') #replace 를 사용하면 위의 문자열을 변하는 게 아니라 aBcd의 새로운 문자열을 생성합니다.
+print(string) # 따라서 abcd 그대로 출력된다.
