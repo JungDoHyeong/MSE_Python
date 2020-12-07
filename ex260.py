4e6e0858cf312a138738e68914e0508255c3a983
JungDoHyeong <ehgud7747@naver.com>
2020-12-07 17:30:02 +0900

    python

---
+++ ex260.py
@@ -0,0 +1,6 @@
+class OMG : 
+    def print() : # print(self)로 바꿔주면 해결할 수있다.
+        print("Oh my god")
+        
+mystock = OMG()
+mystock.print() #OMG.print(mystuck)으로 호출이 된다. 이때 OMG 클래스로 이동된는데 OMG의 인자가 없기 떄문에 오류가 발생힌다.
