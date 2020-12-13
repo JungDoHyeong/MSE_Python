468b519fb2c05ade068c9c4dcf16e63cf7f9db8b
JungDoHyeong <ehgud7747@naver.com>
2020-12-13 20:21:08 +0900

    python

--- ex280.py
+++ ex280.py
@@ -1,66 +1,33 @@
-import random
-
-
-class Account:
-    # class variable
-    account_count = 0 # 
-
+class 예금주:  # 예금주 클래스를 만듦
     def __init__(self, name, balance):
-        self.deposit_count = 0
-        self.deposit_log = []
-        self.withdraw_log = []
-
-        self.name = name
-        self.balance = balance
-        self.bank = "SC은행"
-
-        # 3-2-6
-        num1 = random.randint(0, 999)
-        num2 = random.randint(0, 99)
-        num3 = random.randint(0, 999999)
-
-        num1 = str(num1).zfill(3)  # 1 -> '1' -> '001'
-        num2 = str(num2).zfill(2)  # 1 -> '1' -> '01'
-        num3 = str(num3).zfill(6)  # 1 -> '1' -> '0000001'
-        self.account_number = num1 + '-' + num2 + '-' + num3  # 001-01-000001
-        Account.account_count += 1
-
-    @classmethod
-    def get_account_num(cls):
-        print(cls.account_count)  # Account.account_count
-
-    def deposit(self, amount):
-        if amount >= 1:
-            self.deposit_log.append(amount)
-            self.balance += amount
-
-            self.deposit_count += 1
-            if self.deposit_count % 5 == 0:         # 5, 10, 15
-                # 이자 지금
-                self.balance = (self.balance * 1.01)
+        self.deposit_log = [] # self.deposit_log는 self에 저장되고 리스트로 저장된다.
+        self.withdraw_log = [] #self.withdraw_log는 self에 저장되고 리스트로 저장된다.
+        self.balance = balance 
 
 
-    def withdraw(self, amount):
-        if self.balance > amount:
-            self.withdraw_log.append(amount)
-            self.balance -= amount
+    def deposit(self, amount): # 입금을 할수 있는 사용자 정의 함수를 만든다.
+        if amount >= 1: # 입금할 경우 음의 값을 입금할 수 없으므로 1이상이어야 한다.
+            self.deposit_log.append(amount) # self에 입금한 값을 추가해준다.
+            self.balance += amount # balance에 입금한 값을 더해준다.
 
-    def display_info(self):
-        print("은행이름: ", self.bank)
-        print("예금주: ", self.name)
-        print("계좌번호: ", self.account_number)
-        print("잔고: ", self.balance)
 
-    def withdraw_history(self):
-        for amount in self.withdraw_log:
-            print(amount)
 
-    def deposit_history(self):
+    def withdraw(self, amount): # 출금을 할수 있는 사용자 정의 함수를 만든다.
+        if self.balance > amount: # 출금을 한경우 balance의 값보다 높으면 출금이 불가능하므로 옆의 조건을 건다.
+            self.withdraw_log.append(amount) # self에 출금한 값을 추가해준다.
+            self.balance -= amount # balance에 출금한 값을 빼준다.
+
+
+    def withdraw_history(self): # 출금 기록을 확인할수 있는 사용자 정의 함수를 만든다.
+        for amount in self.withdraw_log: 
+            print(amount) # self.withdraw_log의 입력된 값이 있으면 그 값을 출력하게한다.
+
+    def deposit_history(self): # 입금 기록을 확인할수 있는 사용자 정의 함수를 만든다.
         for amount in self.deposit_log:
-            print(amount)
+            print(amount) # self._log의 입력된 값이 있으면 그 값을 출력하게한다.
 
 
-k = Account("Kim", 1000)
+k = 예금주("Kim", 1000)
 k.deposit(100)
 k.deposit(200)
 k.deposit(300)
